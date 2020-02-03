3f3a32470dbbad07a984d27e2ec30cd4bcd1493a
0xd34df00d <0xd34df00d@gmail.com>
2020-02-02 20:25:41 -0500

    Throw up some tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+{-# LANGUAGE OverloadedStrings, ViewPatterns #-}
+
+import qualified Data.ByteString.Char8 as BS
+import Test.Hspec
+import Test.QuickCheck
+
+import Data.WordCount
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "Some QC properties" $
+    it "Counts ASCII things correctly" $ property $
+      \(getASCIIString -> str) -> wc (BS.pack str) `shouldBe` (length str, length $ words str, length $ filter (== '\n') str)
