e19a16e8fc4d5a575c755ad8fb1889315c4d72c5
0xd34df00d <0xd34df00d@gmail.com>
2020-02-09 19:17:48 -0500

    Add command-line options

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,41 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications, DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE Strict #-}
 
 module Main where
 
-import qualified Data.ByteString.Lazy.Char8 as BS
-import System.Environment
+import Control.Monad
+import Options.Applicative
 import System.IO.Posix.MMap
 
+import Data.Dispatch
 import Data.WordCount
+
+data Options = Options
+  { countBytes :: Bool
+  , countChars :: Bool
+  , countLines :: Bool
+  , countMaxLineLength :: Bool
+  , countWords :: Bool
+  , files :: [FilePath]
+  }
+
+options :: Parser Options
+options = Options
+  <$> switch (long "bytes" <> short 'c' <> help "print the byte counts")
+  <*> switch (long "chars" <> short 'm' <> help "print the character counts")
+  <*> switch (long "lines" <> short 'l' <> help "print the newline counts")
+  <*> switch (long "max-line-length" <> short 'L' <> help "print the maximum display width")
+  <*> switch (long "words" <> short 'w' <> help "print the word counts")
+  <*> some (argument str (metavar "FILES..."))
 
 main :: IO ()
 main = do
-  [path] <- getArgs
-  contents <- unsafeMMapFile path
-  print $ wc @'Words contents
+  Options { .. } <- execParser $ info (options <**> helper) (fullDesc <> progDesc "Print newline, word, and byte counts for each file")
+  let selectedStats = map snd $ filter fst [(countBytes, Bytes), (countWords, Words), (countLines, Lines)]
+  let stats | null selectedStats = [Bytes, Words, Lines]
+            | otherwise = selectedStats
+  forM_ files $ \path -> do
+    contents <- unsafeMMapFile path
+    putStrLn $ $(dispatch 'contents) stats
