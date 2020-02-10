5f8cd505ce0aa8c99e8d260ff12580258d2624bf
0xd34df00d <0xd34df00d@gmail.com>
2020-02-09 19:25:58 -0500

    Fix tests for the new API

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings, ViewPatterns #-}
+{-# LANGUAGE DataKinds, TypeApplications #-}
 
 import qualified Data.ByteString.Char8 as BS
+import Data.List
 import Test.Hspec
 import Test.QuickCheck
 
@@ -8,6 +10,10 @@ import Data.WordCount
 
 main :: IO ()
 main = hspec $
-  describe "Some QC properties" $
-    it "Counts ASCII things correctly" $ property $
-      \(getASCIIString -> str) -> wc (BS.pack str) `shouldBe` (length str, length $ words str, length $ filter (== '\n') str)
+  describe "Some QC properties" $ do
+    it "Counts bytes correctly" $ property $
+      \(getASCIIString -> str) -> wc @'Bytes (BS.pack str) `shouldBe` genericLength str
+    it "Counts words correctly" $ property $
+      \(getASCIIString -> str) -> wc @'Words (BS.pack str) `shouldBe` genericLength (words str)
+    it "Counts lines correctly" $ property $
+      \(getASCIIString -> str) -> wc @'Lines (BS.pack str) `shouldBe` genericLength (filter (== '\n') str)
